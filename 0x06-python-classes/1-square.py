25e9f455b01e3108ce5a3c6c6011c6cdcdecd310
aminem27 <amine_abbes@hotmail.com>
2023-06-27 21:36:54 +0000

    square.py-tasks

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# 0-square.py by Amine Abbes
+"""Define a square """
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size):
+        """Initialize the square class
+        Args: size - the size of the square defined
+        """
+
+        self.__size = size
