fe42f97fbbcbe218654bee93e06f6046b355b5e0
aminem27 <amine_abbes@hotmail.com>
2023-11-13 12:55:11 -0800

    task1-8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++)
+	  console.log('X');
+	console.log('\n');
+  }
+}
